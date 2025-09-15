938e1104883b5c6303baf32ce48a4c421f14bf8e
Pat-devs <patryk.kodehode@gmail.com>
2025-09-15 10:03:38 +0200

    variable examples

--- script.js
+++ script.js
@@ -28,4 +28,25 @@ console.log(-5 + 2)
 // desimaltall fungerer, men vær obs på presisjon hvis dere har mer enn 6 desimaler bak .
 console.log(-0.000000000005 + 1000.0000005)
 
+// lage variabel
+// 1. bruk kodeord let
+// 2. sett et navn på variabelen
+// 3. bruk "assignment operator" (=)
+// 4. set verdi bak = til noe
+let greeting = "Heisann"
+
+console.log(greeting)
+
+console.log(greeting)
+
+// declare a variable and set its value to a number:
+let number1 = 2
+let number2 = 5.25
+
+console.log(number1 * number2 + number1)
+
+// a variable can be almost "anything", even a console.log function
+let minKonsoll = console.log
+
+minKonsoll("hei!")
 
