56ffc822c055ba8636ebc047545710527df754ce
Pat-devs <patryk.kodehode@gmail.com>
2025-09-15 09:55:55 +0200

    js start kode, og tall

---
+++ script.js
@@ -0,0 +1,31 @@
+// Dette er en kode kommentar
+
+// Bruke console.log metoden for å skrive ut meldingen hei
+console.log("hei")
+
+/* Flere linjer med kommentar 
+Kommentar her
+og her 
+og her,
+osv...
+
+*/
+
+// En text i kodespråk er også kjent som en "string" eller "streng på norsk"
+
+// Vi kan bruke enten " eller ' (single eller doublequotes) for å lage strings i Javascript
+console.log('hallo, hvem er du?')
+// Annet eksempel på string:
+console.log("2 + 2")
+
+// tall & matte eksempler:
+console.log(2 + 2)
+console.log(2 * 5)
+console.log(2 / 5)
+console.log(0.5 + 2)
+// Negative tall fungerer også:
+console.log(-5 + 2)
+// desimaltall fungerer, men vær obs på presisjon hvis dere har mer enn 6 desimaler bak .
+console.log(-0.000000000005 + 1000.0000005)
+
+
