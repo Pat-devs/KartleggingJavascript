83cdabf4604fdb5fbdfa3efb257aff67388e64eb
Pat-devs <patryk.kodehode@gmail.com>
2025-09-15 11:32:36 +0200

    generate random color

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
-// how to select a html tag with JS:
-/* With CSS we would:
-h1 {
-  color: red;
-}
-*/
-
-// as above in JS
-// document.querySelector("h1").style = "color: red;"
-
-
-
-// listen to a button click
-
-
 // grab the button element from our page
 let buttonElement = document.querySelector("button")
-// console.log(buttonElement) // sjekk at buttonElement virker, hvis det står null i konsollen da virker den ikke
 
-// create a function that does something
 
-// 1. use the keyword function to declare a function
-// 2. followed by a name of your choice, ex: minFunksjon
-// 3. followed by ()
-// 4. followed by {}
-// function specific code goes inside the { ..  }
-
-function minFunksjon () {
-    console.log("HEIHEI")
-    document.querySelector("h1").style = "color: green;"
-    // here we can do something :)
+function changeH1Color () {
+    randomColor()
+    // document.querySelector("h1").style = "color: green;"
 }
 
-// Gå inni buttonElement
-// Legg til en event-listener
-// Parameter 1: Type of event, in our case "click"
-// Parameter 2: Provide some function to run
-buttonElement.addEventListener("click", minFunksjon)
-
-
-/* 
-document.querySelector("button").addEventListener("click", function() {
-    console.log("Hi")
-})
-*/
+buttonElement.addEventListener("click", changeH1Color)
 
 
 
-// make h1 green
+
+console.log("hi")
+
+console.log(2 - 1)
+
+console.log(buttonElement)
+
+
+// Math.random() gir oss et tall mellom 0.0 og 1.0
+
+// console.log(Math.random() * 100)
+
+// Random color example implementation:
+
+// Color is an RGB value, in css: rgb(0,0,0) - rgb(255,255,255), rgb(255,0,0)
+
+console.log(Math.random() * 255)
+console.log(Math.random() * 255)
+console.log(Math.random() * 255)
+
+function randomColor() {
+
+    let redValue = Math.random() * 255
+    let greenValue = Math.random() * 255
+    let blueValue = Math.random() * 255
+
+    console.log("here is your random color: ")
+    console.log(redValue, greenValue, blueValue)
+
+    document.querySelector("h1").style = "color: rgb(" +  redValue + "," + greenValue + "," + blueValue + ");"
+}
+
+randomColor()
