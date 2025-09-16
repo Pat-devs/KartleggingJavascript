ce6eb9439b48384b60d2706eb24ddad0077451a4
Pat-devs <patryk.kodehode@gmail.com>
2025-09-16 11:20:03 +0200

    implement decrement logic

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // get the current number element
 let currentNumberEl = document.querySelector("#current-number")
-console.log(currentNumberEl)
 // get the button elements and store them as variables, for ease of access
 
-let buttonPlus = document.querySelector("#button-plus")
-let buttonMinus = document.querySelector("#button-minus")
 
 // set number to a value with the assignment operator (=)
 let number = 0
 
+
+
+let buttonPlus = document.querySelector("#button-plus")
 // Function increases the variable called number with + 1
 // And then displays that number in the textContent property of an element called currentNumberEl
 function increase() {
@@ -18,8 +18,18 @@ function increase() {
 
 buttonPlus.addEventListener("click", increase)
 
-// buttonMinus.addEventListener("click", decrease)
 
+
+
+let buttonMinus = document.querySelector("#button-minus")
+// Function increases the variable called number with + 1
+// And then displays that number in the textContent property of an element called currentNumberEl
+function decrease() {
+    number = number - 1 // increment the number
+    currentNumberEl.textContent = number // change the html text to a value
+}
+
+buttonMinus.addEventListener("click", decrease)
 
 // Conditionals in javascript
 
