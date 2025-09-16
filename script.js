91d33967e7a8978615be3385e28ec24552c73bd5
Pat-devs <patryk.kodehode@gmail.com>
2025-09-16 10:26:33 +0200

    increment nums examples

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-
-// Grab the h1 element:
-let h1Element = document.querySelector("h1")
-// Grab the green button element by tagname
-let buttonElementGreen = document.querySelector("#button-green")
-// Grab the red button element by tagname
-let buttonElementRed = document.querySelector("#button-red")
-// Grab the red button element by tagname
-let buttonElementBlack = document.querySelector("#button-blackß")
+// get the button elements and store them as variables, for ease of access
 
 
-function makeItGreen() {
-    // h1Element.style = "color: green;" // bruke class
-    // h1Element.classList.add("green-text")
-    h1Element.className = "green-text" // erstatter klasse med det vi skriver bak = tegn
+let buttonPlus = document.querySelector("#button-plus")
+let buttonMinus = document.querySelector("#button-minus")
+
+// set number to a value with the assignment operator (=)
+let number = 0
+
+
+// increase logic:
+function increase() {
+    console.log("Number is: ",  number)
+    
+    // increment a number in javascript:
+    
+    // different ways to increment a number:
+
+    // number = number + 1
+    // number += 1
+    // number++
+    // ++number
 }
 
-buttonElementGreen.addEventListener("click", makeItGreen)
+buttonPlus.addEventListener("click", increase)
+
+// buttonMinus.addEventListener("click", decrease)
 
 
-function makeItRed() {
-    h1Element.className = "red-text"
+// Conditionals in javascript
+
+
+
+
+
+
+
+
+
+
+
+
+
+// check if number is equal to with the comparison operator (==)
+if (number == 5) {
+    console.log("Great news, the number is 5!")
 }
-
-buttonElementRed.addEventListener("click", makeItRed)
