cb60b8f06a04d45ead5fb0c9208c967e4bf3d82f
Pat-devs <patryk.kodehode@gmail.com>
2025-09-17 10:36:37 +0200

    logical AND, OR

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 
-let userAge = 70
+let userAge = 55
 
 
 // children, adults, seniors
 
 
 // 1. check if user is a child
-if (userAge < 18) {
-    console.log("user is a child")
+// audult er mellom 18 og 59 
+if (userAge >= 18 && userAge <= 59) { // && betyr "og"
+    console.log("user is between 18 and 59 years old")
 }
 else {
-    console.log("User is not a child...")
+    console.log("user is either under 18 or above 59")
+}
 
-    if (userAge > 60) {
-        console.log("user is a senior")
-    }
-    else {
-        console.log("user is an adult")
-    }
+
+// logical or example
+if (userAge <= 17 || userAge >= 60) { // hvis bruker er under 18 eller over 59
+    console.log("user is under 18 or over 59")
 }
