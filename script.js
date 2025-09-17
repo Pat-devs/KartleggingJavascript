45ee2d7f9afb2c8c987c5f7db5d68d4d0bd80863
Pat-devs <patryk.kodehode@gmail.com>
2025-09-17 11:30:04 +0200

    return statement

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
+// Yahtzee
 
-let userAge = 55
+// Rounding methods:
+// Math.round -- Rounds to nearest integer, f.eks: 2.5 = 3, 2.499 = 2 osv...
+// Math.floor -- Rounds Down to nearest integer, f.eks: 2.5 = 2, 2.99999 = 2 osv...
+// Math.ceil -- Rounds Up to nearest integer, f.eks: 2.5 = 3, 2.000001 = 3
+
+// let dice1 = Math.floor( Math.random() * 6 ) // gir tilfeldig heltall mellom 0 og 5
+// let dice2 = Math.ceil( Math.random() * 6 ) // gir tilfeldig heltall mellom 1 og 6
 
 
-// children, adults, seniors
+
+// 1. Can roll a dice
+
+// 2. Can compare different dice rolls?
+
+// 3. Can give score based on dice rolls 
+
+let value1 = 0
+let value2 = 0
 
 
-// 1. check if user is a child
-// audult er mellom 18 og 59 
-if (userAge >= 18 && userAge <= 59) { // && betyr "og"
-    console.log("user is between 18 and 59 years old")
+
+// We want this function to roll a dice result and return it, so that we can do something with it later
+// not just console.log it.
+function diceRoll() {
+
+    let dice = Math.ceil( Math.random() * 6 ) // gir tilfeldig heltall mellom 1 og 6
+    // console.log("Your lucky number is ", dice)
+
+    return dice
+    //value1 = dice
 }
-else {
-    console.log("user is either under 18 or above 59")
+
+// diceRoll()
+
+function giveMe5() {
+    return dice
 }
 
+let something = giveMe5()
 
-// logical or example
-if (userAge <= 17 || userAge >= 60) { // hvis bruker er under 18 eller over 59
-    console.log("user is under 18 or over 59")
-}
+console.log(giveMe5())
+console.log(giveMe5())
+console.log(giveMe5())
+console.log(giveMe5())
