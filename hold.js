a7f687df410a584cb0208521c98c0c02a8cef899
Pat-devs <patryk.kodehode@gmail.com>
2025-09-18 10:07:00 +0200

    cleanup code

--- hold.js
+++ hold.js
@@ -1,36 +1,26 @@
 // single dice object
 let dice = {
-    value: "Hei navnet mitt er Patryk",
+    value: 0,
     roll() {
         this.value = Math.ceil( Math.random() * 6 ) // gir tilfeldig heltall mellom 1 og 6
     }
 }
 
-console.log(dice)
-
-dice.roll()
-
-console.log(dice.value)
-
-// elements
+// dice elements
 let diceAElement = document.querySelector("#dice-a")
+let diceBElement = document.querySelector("#dice-b")
 
-// 
-function testDice() {
-    dice.roll()
-    diceAElement.textContent = dice.value
-}
 
 // diceAElement.addEventListener("click", testDice)
 
 
-let diceBElement = document.querySelector("#dice-b")
-
+// roll dices button
 let rollButton = document.querySelector("#roll")
 
-rollButton.addEventListener("click", testDice)
+// function rolls all available dices
+function rollDices() {
+    dice.roll()
+    diceAElement.textContent = dice.value
+}
 
-
-dice.roll()
-
-console.log(dice.value)
+rollButton.addEventListener("click", rollDices)
