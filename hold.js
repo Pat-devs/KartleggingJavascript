2f5d53bc1f9025c41783bb7ff0cb834da6195251
Pat-devs <patryk.kodehode@gmail.com>
2025-09-18 10:29:40 +0200

    cleanup code

--- hold.js
+++ hold.js
@@ -1,3 +1,8 @@
+// game object (in larger games we would store more than just score)
+let game = {
+    totalScore: 0
+}
+
 // single dice object
 let dice = {
     value: 0,
@@ -6,21 +11,31 @@ let dice = {
     }
 }
 
+// score element
+let scoreElement = document.querySelector("#score")
+
 // dice elements
 let diceAElement = document.querySelector("#dice-a")
 let diceBElement = document.querySelector("#dice-b")
-
-
-// diceAElement.addEventListener("click", testDice)
-
 
 // roll dices button
 let rollButton = document.querySelector("#roll")
 
 // function rolls all available dices
 function rollDices() {
+    // roll the first dice
     dice.roll()
     diceAElement.textContent = dice.value
+    // add the first dice value to the totalScore
+    game.totalScore += dice.value
+
+    // roll the second dice
+    dice.roll()
+    diceBElement.textContent = dice.value
+    // add the second dice value to the totalScore
+    game.totalScore += dice.value
+
+    scoreElement.textContent = game.totalScore
 }
 
 rollButton.addEventListener("click", rollDices)
