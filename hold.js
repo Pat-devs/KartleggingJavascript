bc11643568b8f171e60c94fd6bd741b25d0999f2
Pat-devs <patryk.kodehode@gmail.com>
2025-09-18 11:31:52 +0200

    setup a dice array

--- hold.js
+++ hold.js
@@ -1,21 +1,60 @@
 // game object (in larger games we would store more than just score)
 let game = {
-    totalScore: 0
+    totalScore: 0,
+    diceArray: []
 }
 
-// single dice object
+
+
+
+// single "global" dice object
 let dice = {
     value: 0,
     roll() {
         this.value = Math.ceil( Math.random() * 6 ) // gir tilfeldig heltall mellom 1 og 6
+    },
+    isHeld: false // this value is false by default, except if the player clicks this dice then it is true
+}
+
+// dice factory example
+
+function createDice() {
+    return {
+        value: 0,
+        roll() {
+            this.value = Math.ceil( Math.random() * 6 ) // gir tilfeldig heltall mellom 1 og 6
+        },
+        isHeld: false // this value is false by default, except if the player clicks this dice then it is true
     }
 }
+
+
+
+
+
+
+
+
+
 
 // score element
 let scoreElement = document.querySelector("#score")
 
 // dice elements
 let diceAElement = document.querySelector("#dice-a")
+
+// event handler function for dice A
+function holdDiceA() {
+    console.log("keep dice A!")
+
+    // change background color (To show user this dice is held)
+    diceAElement.style = "background-color: #333;"
+
+    // make the game not roll this dice???
+
+}
+diceAElement.addEventListener("click", holdDiceA)
+
 let diceBElement = document.querySelector("#dice-b")
 
 // roll dices button
@@ -24,6 +63,20 @@ let rollButton = document.querySelector("#roll")
 // function rolls all available dices
 function rollDices() {
     // roll the first dice
+    game.diceArray.push(createDice())
+
+    // check length of the diceArray:
+    let lastDiceIndex = game.diceArray.length - 1// length-1 gives us access to the very last element in the array, which we can then use to "roll the newly created dice" 
+
+    // roll the new dice:
+    game.diceArray[lastDiceIndex].roll() 
+
+    console.log(game.diceArray)
+
+
+    //console.log(game.diceArray)
+
+    /* 
     dice.roll()
     diceAElement.textContent = dice.value
     // add the first dice value to the totalScore
@@ -36,6 +89,9 @@ function rollDices() {
     game.totalScore += dice.value
 
     scoreElement.textContent = game.totalScore
+    */
 }
 
 rollButton.addEventListener("click", rollDices)
+
+
